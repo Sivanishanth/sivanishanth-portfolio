e52ef9eebf088509eff7ea1aca1be2ab18c8dfdf
Sivanishanth <sivanishanth303@gmail.com>
2026-06-10 01:58:54 +0530

    Project Added

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,33 +3,59 @@ import imgStudent from "../assets/images/student-api.png"
 import imgPortfolio from "../assets/images/portfolioLogo.png"
 const projects = [
     {
-        id : 1,
-        title : "Blog API with Authentication",
-        description : "Full-featured blogging backend with JWT authentication, allowing users to create, edit, and delete posts with secure access control",
-        image : imgBlog,
-        tech : ["React" , "Node.js" ,"Express.js" ,"MongoDB" ,"JWT"],
-        github : "https://github.com/Sivanishanth/blog-api",
-        live : "https://sivablog.netlify.app/",
-        feature : true
+        id: 1,
+        title: "Blog API with Authentication",
+        description: "Full-featured blogging backend with JWT authentication, allowing users to create, edit, and delete posts with secure access control",
+        image: imgBlog,
+        tech: ["React", "Node.js", "Express.js", "MongoDB", "JWT"],
+        github: "https://github.com/Sivanishanth/blog-api",
+        live: "https://sivablog.netlify.app/",
+        feature: true
     },
     {
-        id : 2,
-        title : "Student Management REST API",
-        description : "RESTful API for managing student records with CRUD operations and data validation",
-        image : imgStudent,
-        tech : ["Node.js" ,"Express.js","MongoDB","Mongoose"],
-        github : "https://github.com/Sivanishanth/student-api",
-        feature : true
+        id: 2,
+        title: "Student Management REST API",
+        description: "RESTful API for managing student records with CRUD operations and data validation",
+        image: imgStudent,
+        tech: ["Node.js", "Express.js", "MongoDB", "Mongoose"],
+        github: "https://github.com/Sivanishanth/student-api",
+        feature: true
     },
     {
-        id : 3,
-        title : " My Portfolio Website",
-        description : "A responsive portfolio website showcasing my projects and skills.",
-        image : imgPortfolio,
-        tech : ["React" ,"HTML", "CSS" ,"JavaScript"],
-        github : "https://github.com/Sivanishanth/myPortfolio",
-        live : "https://sivanishanth-portfolio.netlify.app/",
-        feature : true
+        id: 3,
+        title: " My Portfolio Website",
+        description: "A responsive portfolio website showcasing my projects and skills.",
+        image: imgPortfolio,
+        tech: ["React", "HTML", "CSS", "JavaScript"],
+        github: "https://github.com/Sivanishanth/myPortfolio",
+        live: "https://sivanishanth-portfolio.netlify.app/",
+        feature: true
+    },
+    {
+        id: 4,
+        title: "DevConnect - Full-Stack Developer Portfolio Platform",
+        description: "A responsive portfolio website showcasing my projects and skills.",
+        image: imgDevConnect,
+        tech: ["React.js", "Node.js",
+            "Express.js",
+            "MongoDB",
+            "JavaScript (ES6+)",
+            "Tailwind CSS",
+            "Vite",
+            "JWT Authentication",
+            "Bcrypt",
+            "Axios",
+            "REST API",
+            "MongoDB Mongoose",
+            "React Router",
+            "Context API",
+            "Responsive Design",
+            "Git & GitHub",
+            "Render",
+            "Netlify"],
+        github: "https://github.com/Sivanishanth/DevConnect",
+        live: "https://dvconnect.netlify.app",
+        feature: true
     }
 ]
 
